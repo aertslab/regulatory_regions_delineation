bc6243c9f747418ec09b64df3995e754a6b63529
Gert Hulselmans <gert.hulselmans@kuleuven.vib.be>
2017-02-09 17:06:22 +0100

    Rewrite create-genes-database.sh: add comments and make the code more readable.
    Rewrite create-genes-database.sh: add comments and make the code
    more readable.
    
    Signed-off-by: Gert Hulselmans <gert.hulselmans@kuleuven.vib.be>

--- create-genes-database.sh
+++ create-genes-database.sh
@@ -1,28 +1,124 @@
 #!/usr/bin/env bash
 
-if [ $# -ne 3 ]; then
-	echo "Wrong number of input arguments."
-	echo
-	echo "Usage: bash create-genes-database.sh <table_filename> <genome_2bit_filename> <database_name>"
-	exit 2
+if [ ${#@} -ne 3 ]; then
+	printf 'Wrong number of input arguments.\n\n';
+	printf 'Usage:\n    bash create-genes-database.sh <refgene_table_filename> <genome_2bit_filename> <database_name>\n\n';
+	printf 'Link to input files:\n';
+	printf '    - refgene_table_filename:  ftp://hgdownload.cse.ucsc.edu/goldenPath/<assembly>/database/refGene.txt.gz\n';
+	printf '    - genome_2bit_filename:    ftp://hgdownload.cse.ucsc.edu/goldenPath/<assembly>/bigZips/<assembly>.2bit\n\n';
+	exit 2;
 fi
-TABLE_FILENAME=$1
-GENOME_FILENAME=$2
-DATABASE_FILENAME=$3
 
-rm -f ${DATABASE_FILENAME}
-# Only keep columns: name, chrom, strand, txStart, txEnd, cdsStart, cdsEnd, exonCount, exonStarts, exonEnds, name2
-cat ${TABLE_FILENAME} | sed '1d' | cut -f2,3,4,5,6,7,8,9,10,11,13 | tr '\t' '|' > sqlite3-genes-import.psv
-# Find length of chromosomes ...
-twoBitInfo ${GENOME_FILENAME} stdout | tr '\t' '|' > sqlite3-chromosomes-import.psv
-sqlite3 -line ${DATABASE_FILENAME} "create table genes ( ID varchar(255), \
-chromosome varchar(255), strand char(1), txStart integer, txEnd integer, \
-cdsStart integer, cdsEnd integer, exonCount integer, exonStarts blob, exonEnds blob, altName varchar(255));"
-sqlite3 -separator '|' -line ${DATABASE_FILENAME} ".import sqlite3-genes-import.psv genes"
-sqlite3 -line ${DATABASE_FILENAME} "create index ID on genes ( ID );"
-sqlite3 -line ${DATABASE_FILENAME} "create index altName on genes ( altName );"
-sqlite3 -line ${DATABASE_FILENAME} "create index location on genes ( chromosome, strand, txStart, txEnd );"
-sqlite3 -line ${DATABASE_FILENAME} "create table chromosomes ( name varchar(255), length integer);"
-sqlite3 -separator '|' -line ${DATABASE_FILENAME} ".import sqlite3-chromosomes-import.psv chromosomes"
-sqlite3 -line ${DATABASE_FILENAME} "create index name on chromosomes ( name );"
-rm -f sqlite3-genes-import.psv sqlite3-chromosomes-import.psv
+REFGENE_TABLE_FILENAME="${1}";
+GENOME_2BIT_FILENAME="${2}";
+DATABASE_FILENAME="${3}";
+
+
+# Check if all input files exist and have the right extension,
+# so there might be a high chance that the correct input files are used.
+
+if [ ! -f "${REFGENE_TABLE_FILENAME}" ] ; then
+    printf 'ERROR: RefGene Table file "%s" could not be found.\n' "${REFGENE_TABLE_FILENAME}";
+    exit 1;
+fi
+
+if [ "${REFGENE_TABLE_FILENAME}" = "${REFGENE_TABLE_FILENAME%.txt.gz}" ] ; then
+    printf 'ERROR: RefGene Table file "%s" should end with ".txt.gz".\n' "${REFGENE_TABLE_FILENAME}";
+    exit 1;
+fi
+
+if [ ! -f "${GENOME_2BIT_FILENAME}" ] ; then
+    printf 'ERROR: Genome 2bit file "%s" could not be found.\n' "${GENOME_2BIT_FILENAME}";
+    exit 1;
+fi
+
+if [ "${GENOME_2BIT_FILENAME}" = "${GENOME_2BIT_FILENAME%.2bit}" ] ; then
+    printf 'ERROR: Genome 2bit file "%s" should end with ".2bit".\n' "${GENOME_2BIT_FILENAME}";
+    exit 1;
+fi
+
+if [ $(type sqlite3 > /dev/null 2>&1; echo $?;) -ne 0 ] ; then
+    printf 'ERROR: Add "sqlite3" to ${PATH}.\n';
+    exit 1;
+fi
+
+if [ $(type twoBitInfo > /dev/null 2>&1; echo $?;) -ne 0 ] ; then
+    printf 'ERROR: Add "twoBitInfo" to ${PATH}.\n';
+    exit 1;
+fi
+
+
+# Specify all SQL queries.
+
+# Original SQL scheme for refGene table can be found at:
+#   ftp://hgdownload.cse.ucsc.edu/goldenPath/<assembly>/database/refGene.sql
+
+CREATE_TABLE_GENES_QUERY='
+CREATE TABLE `genes` (
+    `geneID` VARCHAR(255) NOT NULL,
+    `chromosome` VARCHAR(255) NOT NULL,
+    `strand` CHAR(1) NOT NULL,
+    `txStart` INTEGER NOT NULL,
+    `txEnd` INTEGER NOT NULL,
+    `cdsStart` INTEGER NOT NULL,
+    `cdsEnd` INTEGER NOT NULL,
+    `exonCount` INTEGER NOT NULL,
+    `exonStarts` LONGBLOB NOT NULL,
+    `exonEnds` LONGBLOB NOT NULL,
+    `geneName` VARCHAR(255) NOT NULL
+);
+'
+
+CREATE_INDICES_FOR_GENES_TABLE_QUERY='
+CREATE INDEX `geneID` ON `genes` (`geneID`);
+CREATE INDEX `geneName` ON `genes` (`geneName`);
+CREATE INDEX `location` ON `genes` (`chromosome`, `strand`, `txStart`, `txEnd`);
+';
+
+CREATE_TABLE_CHROMOSOMES_QUERY='
+CREATE TABLE `chromosomes` (
+    `name` VARCHAR(255) NOT NULL,
+    `length` INTEGER NOT NULL
+);'
+
+CREATE_INDEX_FOR_CHROMOSOMES_TABLE_QUERY='
+CREATE INDEX `name` ON `chromosomes` (`name`);
+'
+
+
+# Remove old database file, if it exists.
+rm -f "${DATABASE_FILENAME}";
+
+
+# Create "genes" and "chromsomes" tables.
+sqlite3 -line \
+    "${DATABASE_FILENAME}" \
+    "${CREATE_TABLE_GENES_QUERY}${CREATE_TABLE_CHROMOSOMES_QUERY}"
+
+
+# Import refGene data in "genes" table:
+#   - Only keep the following columns:
+#       name, chrom, strand, txStart, txEnd, cdsStart, cdsEnd, exonCount, exonStarts, exonEnds, name2
+zcat refGene.txt.gz \
+    | awk -F '\t' -v 'OFS=\t' '{ print $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $13; }' \
+    | sqlite3 \
+        -separator $'\t' \
+        -line \
+        "${DATABASE_FILENAME}" \
+        '.import /dev/stdin genes';
+
+
+# Import chromosome name and chromosome size data in "chromosomes" tables.
+twoBitInfo "${GENOME_2BIT_FILENAME}" stdout \
+    | sqlite3 \
+        -separator $'\t' \
+        -line \
+        "${DATABASE_FILENAME}" \
+        '.import /dev/stdin chromosomes';
+
+
+# Create indices for "genes" and "chromosomes" tables.
+sqlite3 \
+    -line \
+    "${DATABASE_FILENAME}" \
+    "${CREATE_INDICES_FOR_GENES_TABLE_QUERY}${CREATE_INDEX_FOR_CHROMOSOMES_TABLE_QUERY}";
