baaf632b3f9420d5d03d15605b0a257a83147bff
Gert Hulselmans <gert.hulselmans@kuleuven.vib.be>
2017-02-10 14:24:50 +0100

    Update generate-gene-region-table.py to python3 and rename some variables.
    Update generate-gene-region-table.py to python3 and rename some variables.
    
    Signed-off-by: Gert Hulselmans <gert.hulselmans@kuleuven.vib.be>

--- generate-gene-region-table.py
+++ generate-gene-region-table.py
@@ -1,26 +1,29 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import sys
 
 
 def bedfile_iterator(filename):
     with open(filename, 'r') as handle:
         for line in handle:
-             # Only keep 4th column of BED file with ID information ...
+            # Only keep 4th column of BED file with region ID information ...
             region_name = line.rstrip().split('\t')[3]
-            id, nr = region_name.split('#')
-            yield region_name, id
+            gene_name, nr = region_name.split('#')
+            yield region_name, gene_name
 
 
 def main():
     if len(sys.argv) != 2:
-        print "Wrong number of input arguments."
+        print('Wrong number of input arguments.',
+              'Usage: {0:s} bed_filename > gene_name_to_region_names.tsv'.format(sys.argv[0]),
+              sep='\n',
+              file=sys.stderr)
         sys.exit(2)
-        
-    id2regions = dict()
-    for region, id in bedfile_iterator(sys.argv[1]):
-        id2regions.setdefault(id, set()).add(region)
-    for id in sorted(id2regions.keys()):
-        print id + "\t" + "\t".join(sorted(id2regions[id])) 
+
+    gene_name2region_names = dict()
+    for region_name, gene_name in bedfile_iterator(sys.argv[1]):
+        gene_name2region_names.setdefault(gene_name, set()).add(region_name)
+    for gene_name in sorted(gene_name2region_names.keys()):
+        print(gene_name + "\t" + "\t".join(sorted(gene_name2region_names[gene_name])))
 
 
 if __name__ == "__main__":
