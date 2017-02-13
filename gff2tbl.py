3d66aabaaa4c33137bfd823b3f90ffb297db3bde
Gert Hulselmans <gert.hulselmans@kuleuven.vib.be>
2017-02-13 16:56:31 +0100

    Update gff2tbl.py to PEP8 and rename some variables and update code to python3.
    Update gff2tbl.py to PEP8 and rename some variables and update code to python3.
    
    Signed-off-by: Gert Hulselmans <gert.hulselmans@kuleuven.vib.be>

--- gff2tbl.py
+++ gff2tbl.py
@@ -1,131 +1,153 @@
 #!/usr/bin/env python
-import sys, operator
+import operator
+import sys
 
-
-INCLUDE_TYPES = ('mRNA', 'miRNA', 'ncRNA', 'tRNA', 'rRNA', 'snRNA', 'snoRNA', 'CDS', 'exon')
+INCLUDE_FEATURE_TYPES = ('mRNA', 'miRNA', 'ncRNA', 'tRNA', 'rRNA', 'snRNA', 'snoRNA', 'CDS', 'exon')
 
 
 def iterate_gff_body(filename):
     with open(filename, 'r') as handle:
         for line in handle:
-            if line.startswith('###'): return
-            if line.startswith('##'): continue
-            seqid, source, type, start, end, score, strand, phase, attributes_str = line.rstrip().split('\t')
-            if type not in INCLUDE_TYPES: continue
+            if line.startswith('###'):
+                return
+            if line.startswith('##'):
+                continue
+            seq_id, source, feature_type, start, end, score, strand, phase, attributes_str = line.rstrip().split('\t')
+            if feature_type not in INCLUDE_FEATURE_TYPES:
+                continue
             attributes = dict()
             for element in attributes_str.split(';'):
                 key, value = element.split('=')
-                if key == 'Parent': attributes[key] = value.split(',')
-                elif key == 'ID': attributes[key] = value
-                elif key == 'Name': attributes[key] = value
+                if key == 'Parent':
+                    attributes[key] = value.split(',')
+                elif key == 'ID':
+                    attributes[key] = value
+                elif key == 'Name':
+                    attributes[key] = value
             # Make intervals half-open and zero-based (in GFF they are closed intervals and 1-based) ...
-            yield type, seqid, int(start)-1, int(end), strand, attributes
+            yield feature_type, seq_id, int(start) - 1, int(end), strand, attributes
 
-    
+
 class Entry:
     @staticmethod
-    def from_mRNA(ID, chromosome, tx, strand, geneID):
-        entry = Entry(ID)
-        entry.chromosome, entry.tx, entry.strand, entry.geneID = chromosome, tx, strand, geneID
+    def from_mRNA(feature_id, chromosome, tx, strand, gene_id):
+        entry = Entry(feature_id)
+        entry.chromosome, entry.tx, entry.strand, entry.gene_id = chromosome, tx, strand, gene_id
         return entry
-    
-    def __init__(self, ID):
-        self.ID, self.chromosome, self.tx, self.strand, self.CDSs, self.exons, self.geneID = ID, '', (), '', [], [], ''
-    
+
+    def __init__(self, feature_id):
+        self.feature_id, self.chromosome, self.tx, self.strand, self.CDSs, self.exons, self.gene_id = \
+            feature_id, '', (), '', [], [], ''
+
     def isempty(self):
         return len(self.tx) == 0
-    
+
     def exon_count(self):
         return len(self.exons)
-    
+
     def exon_starts(self):
-        if self.exon_count() == 0: return tuple()
+        if self.exon_count() == 0:
+            return tuple()
         return sorted(map(operator.itemgetter(0), self.exons))
-        
+
     def exon_ends(self):
-        if self.exon_count() == 0: return tuple()
+        if self.exon_count() == 0:
+            return tuple()
         return sorted(map(operator.itemgetter(1), self.exons))
-    
+
     def cds_span(self):
-        if len(self.CDSs) == 0: return self.tx[1], self.tx[1]
-        return min(map(operator.itemgetter(0), self.CDSs)), max(map(operator.itemgetter(1), self.CDSs))            
+        if len(self.CDSs) == 0:
+            return self.tx[1], self.tx[1]
+        return min(map(operator.itemgetter(0), self.CDSs)), max(map(operator.itemgetter(1), self.CDSs))
 
 
-def load_data(filename):
-    ID2entry = dict()
-    for type, chromosome, start, end, strand, attributes in iterate_gff_body(filename):
-        if type.endswith('RNA'):
-            #Analyze attributes ...
+def load_data(gff_filename):
+    feature_id2entry = dict()
+    for feature_type, chromosome, start, end, strand, attributes in iterate_gff_body(gff_filename):
+        if feature_type.endswith('RNA'):
+            # Analyze attributes ...
             if 'ID' not in attributes:
-                print >>sys.stderr, "'{0:s}' entry (ID: {1:s}) has no ID attribute.".format(type, ID)
+                print("'{0:s}' entry ({1:s} {2:d} {3:d} {4:s}) has no ID attribute.".format(feature_type, chromosome,
+                                                                                            start, end, strand),
+                      file=sys.stderr)
                 sys.exit(1)
-            ID = attributes['ID']
+            feature_id = attributes['ID']
             if 'Parent' in attributes and len(attributes['Parent']) == 1:
-                geneID = attributes['Parent'][0]
-            elif 'Name' in attributes: geneID = attributes['Name']
-            else:    
-                print >>sys.stderr, "'{0:s}' entry (ID: {1:s}) cannot be associated with a geneID.".format(type, ID)
+                gene_id = attributes['Parent'][0]
+            elif 'Name' in attributes:
+                gene_id = attributes['Name']
+            else:
+                print("'{0:s}' entry (ID: {1:s}) cannot be associated with a geneID.".format(feature_type, feature_id),
+                      file=sys.stderr)
                 sys.exit(1)
-            
-            if ID in ID2entry.keys():
-                #if ID2entry[ID].chromosome != chromosome or ID2entry[ID].strand != strand:
-                #    print >>sys.stderr, "'mRNA' entry not compatible with child entry."
-                #    sys.exit(1)
-                ID2entry[ID].chromosome = chromosome
-                ID2entry[ID].tx = (start, end)
-                ID2entry[ID].strand = strand
-                ID2entry[ID].geneID = geneID
-            else: ID2entry[ID] = Entry.from_mRNA(ID, chromosome, (start, end), strand, geneID)
-        elif type == 'exon':
+
+            if feature_id in feature_id2entry.keys():
+                # if feature_id2entry[feature_id].chromosome != chromosome or feature_id2entry[feature_id].strand != strand:
+                #     print("'mRNA' entry not compatible with child entry.", file=sys.stderr)
+                #     sys.exit(1)
+                feature_id2entry[feature_id].chromosome = chromosome
+                feature_id2entry[feature_id].tx = (start, end)
+                feature_id2entry[feature_id].strand = strand
+                feature_id2entry[feature_id].gene_id = gene_id
+            else:
+                feature_id2entry[feature_id] = Entry.from_mRNA(feature_id, chromosome, (start, end), strand, gene_id)
+        elif feature_type == 'exon':
             if 'Parent' not in attributes:
-                print >>sys.stderr, "'exon' entry (ID: {0:s}) has no Parent attribute.".format(ID)
+                print("'exon' entry (ID: {0:s}) has no Parent attribute.".format(feature_id), file=sys.stderr)
                 sys.exit(1)
             exon = (start, end)
-            for parentID in attributes['Parent']:
-                if parentID not in ID2entry.keys(): ID2entry[parentID] = Entry(parentID)
-                #elif ID2entry[parentID].chromosome != chromosome or ID2entry[parentID].strand != strand:
-                #    print >>sys.stderr, "'exon' entry not compatible with parent 'mRNA' entry {0:s}.".format(parentID)
-                #    sys.exit(1)
-                ID2entry[parentID].exons.append(exon)
-                #ID2entry[parentID].chromosome = chromosome
-                #ID2entry[parentID].strand = strand
-        elif type == 'CDS':
+            for parent_id in attributes['Parent']:
+                if parent_id not in feature_id2entry.keys():
+                    feature_id2entry[parent_id] = Entry(parent_id)
+                # elif feature_id2entry[parent_id].chromosome != chromosome or feature_id2entry[parent_id].strand != strand:
+                #     print("'exon' entry not compatible with parent 'mRNA' entry {0:s}.".format(parent_id),
+                #           file=sys.stderr)
+                #     sys.exit(1)
+                feature_id2entry[parent_id].exons.append(exon)
+                # feature_id2entry[parent_id].chromosome = chromosome
+                # feature_id2entry[parent_id].strand = strand
+        elif feature_type == 'CDS':
             if 'Parent' not in attributes:
-                print >>sys.stderr, "'CDS' entry (ID: {0:s}) has no Parent attribute.".format(ID)
+                print("'CDS' entry (ID: {0:s}) has no Parent attribute.".format(feature_id), file=sys.stderr)
                 sys.exit(1)
             CDS = (start, end)
-            for parentID in attributes['Parent']:
-                if parentID not in ID2entry.keys(): ID2entry[parentID] = Entry(parentID)
-                #elif ID2entry[parentID].chromosome != chromosome or ID2entry[parentID].strand != strand:
-                #    print >>sys.stderr, "'CDS' entry not compatible with parent 'mRNA' entry {0:s}.".format(parentID)
-                #    sys.exit(1)
-                ID2entry[parentID].CDSs.append(CDS)
-                #ID2entry[parentID].chromosome = chromosome
-                #ID2entry[parentID].strand = strand
-    return ID2entry
+            for parent_id in attributes['Parent']:
+                if parent_id not in feature_id2entry.keys():
+                    feature_id2entry[parent_id] = Entry(parent_id)
+                # elif feature_id2entry[parent_id].chromosome != chromosome or feature_id2entry[parent_id].strand != strand:
+                #     print("'CDS' entry not compatible with parent 'mRNA' entry {0:s}.".format(parent_id),
+                #           file=sys.stderr)
+                #     sys.exit(1)
+                feature_id2entry[parent_id].CDSs.append(CDS)
+                # feature_id2entry[parent_id].chromosome = chromosome
+                # feature_id2entry[parent_id].strand = strand
+    return feature_id2entry
 
 
 def main():
     if len(sys.argv) != 2:
-        print >>sys.stderr, "Wrong number of input arguments."
+        print('Wrong number of input arguments.', file=sys.stderr)
         sys.exit(2)
-    filename = sys.argv[1]
-    print >>sys.stderr, "Loading GFF into memory ..."
-    ID2entry = load_data(filename)
-    print >>sys.stderr, "Conversion to tbl format ..."
-    print >>sys.stdout, "#bin\tname\tchrom\tstrand\ttxStart\ttxEnd\tcdsStart\tcdsEnd\texonCount\texonStarts\texonEnds\tscore\tname2\tcdsStartStat\tcdsEndStat\texonFrames"
-    for ID, entry in ID2entry.iteritems():
+    gff_filename = sys.argv[1]
+    print('Loading GFF into memory ...', file=sys.stderr)
+    feature_id2entry = load_data(gff_filename)
+    print('Conversion to tbl format ...', file=sys.stderr)
+    print('#bin', 'name', 'chrom', 'strand', 'txStart', 'txEnd', 'cdsStart', 'cdsEnd', 'exonCount',
+          'exonStarts', 'exonEnds', 'score', 'name2', 'cdsStartStat', 'cdsEndStat', 'exonFrames',
+          sep='\t')
+
+    for feature_id, entry in feature_id2entry.items():
         if entry.isempty():
-            print >>sys.stderr, "{0:s} is an empty entry.".format(ID)
+            print('{0:s} is an empty entry.'.format(feature_id), file=sys.stderr)
             continue
-        if entry.exon_count() == 0: entry.exons.append(entry.tx)
+        if entry.exon_count() == 0:
+            entry.exons.append(entry.tx)
         cds = entry.cds_span()
-        exonStarts = ','.join(map(str,entry.exon_starts())) + ','
-        exonEnds = ','.join(map(str,entry.exon_ends())) + ','
-        print >>sys.stdout, "NA\t{0:s}\t{1:s}\t{2:s}\t{3:d}\t{4:d}\t{5:d}\t{6:d}\t{7:d}\t{8:s}\t{9:s}\tNA\t{10:s}\tNA\tNA\tNA".format(
-                ID, entry.chromosome, entry.strand, entry.tx[0], entry.tx[1], cds[0], cds[1],
-                entry.exon_count(), exonStarts, exonEnds,
-                entry.geneID)
+        exon_starts = ','.join(map(str, entry.exon_starts())) + ','
+        exon_ends = ','.join(map(str, entry.exon_ends())) + ','
+        print('NA', feature_id, entry.chromosome, entry.strand, entry.tx[0], entry.tx[1], cds[0], cds[1],
+              entry.exon_count(), exon_starts, exon_ends, 'NA', entry.gene_id, 'NA', 'NA', 'NA',
+              sep='\t')
 
 
 if __name__ == "__main__":
