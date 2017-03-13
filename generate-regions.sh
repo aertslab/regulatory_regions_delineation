e362209650a9590e126fb283c5d744a65081a07e
Gert Hulselmans <gert.hulselmans@kuleuven.vib.be>
2017-03-13 15:43:51 +0100

    Cleanup code in generate-regions.sh and exit when a mayor error occurs.
    Cleanup code in generate-regions.sh and exit when a mayor error occurs.
    
    Signed-off-by: Gert Hulselmans <gert.hulselmans@kuleuven.vib.be>

--- generate-regions.sh
+++ generate-regions.sh
@@ -172,60 +172,197 @@ create_filename() {
 
 # Create SQLite3 gene database ...
 printf '\nCreate SQLite3 gene database ...\n\n';
-db_filename=$(create_filename ${base_genome_id} ${output_dir} ${upstream_extension_in_bp} ${delineation} ${downstream_extension_in_bp} "sqlite3.db")
-bash create-genes-database.sh ${ucsc_table} ${base_genome_2bit_file} ${db_filename}
+
+db_filename=$(
+    create_filename \
+        "${base_genome_id}" \
+        "${output_dir}" \
+        "${upstream_extension_in_bp}" \
+        "${delineation}" \
+        "${downstream_extension_in_bp}" \
+        'sqlite3.db'
+);
+
+bash create-genes-database.sh "${ucsc_table}" "${base_genome_2bit_file}" "${db_filename}" || exit 1;
+
 
 # Create BED file ...
 printf '\nCreate BED file ...\n\n';
-bed_filename=$(create_filename ${base_genome_id} ${output_dir} ${upstream_extension_in_bp} ${delineation} ${downstream_extension_in_bp} "bed")
-echo "${chromosomes}" | tr ';' '\n' > $output_dir/chromosomes.tmp
-python create-regulatory-regions-bed.py ${db_filename} $output_dir/chromosomes.tmp ${upstream_extension_in_bp} ${downstream_extension_in_bp} ${intronic_extension_in_bp} ${delineation} > ${bed_filename}
-rm -f $output_dir/chromosomes.tmp
+
+bed_filename=$(
+    create_filename \
+        "${base_genome_id}" \
+        "${output_dir}"
+        "${upstream_extension_in_bp}" \
+        "${delineation}" \
+        "${downstream_extension_in_bp}" \
+        'bed'
+);
+
+echo "${chromosomes}" | tr ';' '\n' > "${output_dir}/chromosomes.tmp";
+
+python create-regulatory-regions-bed.py \
+    "${db_filename}" \
+    "${output_dir}/chromosomes.tmp" \
+    "${upstream_extension_in_bp}" \
+    "${downstream_extension_in_bp}" \
+    "${intronic_extension_in_bp}" \
+    "${delineation}" \
+    > "${bed_filename}";
+
+rm -f "${output_dir}/chromosomes.tmp";
+
 
 # Create distribution of length of search space ...
-figure_filename=$(create_filename ${base_genome_id} ${output_dir} ${upstream_extension_in_bp} ${delineation} ${downstream_extension_in_bp} "png")
-/.generate-histogram.sh ${bed_filename}
+figure_filename=$(
+    create_filename \
+        "${base_genome_id}" \
+        "${output_dir}" \
+        "${upstream_extension_in_bp}" \
+        "${delineation}" \
+        "${downstream_extension_in_bp}" \
+        'png'
+);
+
+bash generate-histogram.sh "${bed_filename}" || exit 1;
+
 
 # Create gene ID file and gene description table ...
 printf '\nCreate gene ID file and gene description table ...\n\n';
-id_filename=$(create_filename ${base_genome_id} ${output_dir} ${upstream_extension_in_bp} ${delineation} ${downstream_extension_in_bp} "gene-ids")
-cat ${bed_filename} | cut -f4 | cut -d'#' -f1 | sort -u > ${id_filename}
-description_table_filename=$(create_filename ${base_genome_id} ${output_dir} ${upstream_extension_in_bp} ${delineation} ${downstream_extension_in_bp} "gene-descriptions")
-cat ${id_description_table} | sort -k1 | join -a 1 -1 1 -2 1 ${id_filename} - | sed -e 's/ /|/' | tr '|' '\t' > ${description_table_filename}
-regions_filename=$(create_filename ${base_genome_id} ${output_dir} ${upstream_extension_in_bp} ${delineation} ${downstream_extension_in_bp} "regions")
-cat ${bed_filename} | cut -f4 | sort -u > ${regions_filename}
-region_table_filename=$(create_filename ${base_genome_id} ${output_dir} ${upstream_extension_in_bp} ${delineation} ${downstream_extension_in_bp} "gene-regions")
-python generate-gene-region-table.py ${bed_filename} > ${region_table_filename}
+
+gene_ids_filename=$(
+    create_filename \
+        "${base_genome_id}" \
+        "${output_dir}" \
+        "${upstream_extension_in_bp}" \
+        "${delineation}" \
+        "${downstream_extension_in_bp}" \
+        'gene-ids'
+);
+
+cut -f4 "${bed_filename}" | cut -d'#' -f1 | sort -u > "${gene_ids_filename}";
+
+description_table_filename=$(
+    create_filename \
+        "${base_genome_id}" \
+        "${output_dir}" \
+        "${upstream_extension_in_bp}" \
+        "${delineation}" \
+        "${downstream_extension_in_bp}" \
+        'gene-descriptions'
+);
+
+sort -k 1,1 "${id_description_table}" \
+    | join -t $'\t' -a 1 -1 1 -2 1 "${gene_ids_filename}" - \
+    > "${description_table_filename}";
+
+regions_filename=$(
+    create_filename \
+        "${base_genome_id}" \
+        "${output_dir}" \
+        "${upstream_extension_in_bp}" \
+        "${delineation}" \
+        "${downstream_extension_in_bp}" \
+        'regions'
+);
+
+cut -f 4 "${bed_filename}" | sort -u > "${regions_filename}";
+
+region_table_filename=$(
+    create_filename \
+        "${base_genome_id}" \
+        "${output_dir}" \
+        "${upstream_extension_in_bp}" \
+        "${delineation}" \
+        "${downstream_extension_in_bp}" \
+        'gene-regions'
+);
+
+python generate-gene-region-table.py "${bed_filename}" > "${region_table_filename}" || exit 1;
+
 
 # Analysis of lost gene IDs ...
 printf '\nAnalysis of lost gene IDs ...\n\n';
-lostids_filename=$(create_filename ${base_genome_id} ${output_dir} ${upstream_extension_in_bp} ${delineation} ${downstream_extension_in_bp} "lost-gene-ids")
-cat ${ucsc_table} | sed '1d' | cut -f13 | sort -u > $output_dir/geneids-all.tmp
-grep -vxF -f ${id_filename} $output_dir/geneids-all.tmp > ${lostids_filename}
-rm -f $output_dir/geneids-all.tmp
-nr_of_lost_genes=`cat ${lostids_filename} | wc -l`
-echo "Lost ${nr_of_lost_genes}# genes ..."
 
-# Create i-cisTarget ini file ...
-printf '\nCreate i-cisTarget ini file ...\n\n';
-full_output_dir=`cd ${output_dir}; pwd`
-full_regions_filename=`python -c "import os.path; print os.path.realpath('${regions_filename}');"`
-full_region_table_filename=`python -c "import os.path; print os.path.realpath('${region_table_filename}');"`
-cat cistargetx-template.ini \
-		| sed -e "s!HOME!${HOME}!; s!DATADIR!${DATADIR}!; s!OUTPUTDIR!${full_output_dir}!; s!REGIONSFILE!${full_regions_filename}!; s!LUTFILE!${full_region_table_filename}!" \
-		> ${output_dir}/cistargetx-template.ini
+lost_gene_ids_filename=$(
+    create_filename \
+        "${base_genome_id}" \
+        "${output_dir}" \
+        "${upstream_extension_in_bp}" \
+        "${delineation}" \
+        "${downstream_extension_in_bp}" \
+        'lost-gene-ids'
+);
+
+all_gene_ids_filename=$(
+    create_filename \
+        "${base_genome_id}" \
+        "${output_dir}" \
+        "${upstream_extension_in_bp}" \
+        "${delineation}" \
+        "${downstream_extension_in_bp}" \
+        'all-gene-ids'
+);
+
+grep -v '^#' "${ucsc_table}" | cut -f13 | sort -u > "${all_gene_ids_filename}";
+grep -vxF -f "${gene_ids_filename}" "${all_gene_ids_filename}" > "${lost_gene_ids_filename}";
+nr_of_lost_genes=$(cat "${lost_gene_ids_filename}" | wc -l);
+echo "Lost ${nr_of_lost_genes}# genes ...";
+
+
+# Create i-cisTarget ctx-pwm2r ini file ...
+printf '\nCreate i-cisTarget ctx-pwm2r ini file ...\n\n';
+
+full_output_dir=$(cd ${output_dir}; pwd);
+full_regions_filename=$(python -c "import os.path; print os.path.realpath('${regions_filename}');");
+full_region_table_filename=$(python -c "import os.path; print os.path.realpath('${region_table_filename}');");
+
+ctx_pwm2r_filename=$(
+    create_filename \
+        "ctx_pwm2r.${base_genome_id}" \
+        "${output_dir}" \
+        "${upstream_extension_in_bp}" \
+        "${delineation}" \
+        "${downstream_extension_in_bp}" \
+        'all-gene-ids'
+);
+
+cat ctx-pwm2r-template.ini \
+    | sed -e "s!OUTPUT_DIR!${full_output_dir}!; s!REGIONS_FILE!${full_regions_filename}!; s!LUT_FILE!${full_region_table_filename}!" \
+    > "${ctx_pwm2r_filename}";
+
 
 # Create FASTA file ...
 printf '\nCreate FASTA file ...\n\n';
-fasta_filename=$(create_filename ${base_genome_id} ${output_dir} ${upstream_extension_in_bp} ${delineation} ${downstream_extension_in_bp} "fasta")
-twoBitToFa -bed=${bed_filename} ${base_genome_2bit_file} ${fasta_filename}
-#Usage of -bed instead of -seqList is easier and safe ...
-#awk '{printf "%s:%d-%d\n", $1, $2, $3}' ${bed_filename} | twoBitToFa ${base_genome_2bit_file} ${fasta_filename} -seqList=stdin
+
+fasta_filename=$(
+    create_filename \
+        "${base_genome_id}" \
+        "${output_dir}" \
+        "${upstream_extension_in_bp}" \
+        "${delineation}" \
+        "${downstream_extension_in_bp}" \
+        'fasta'
+);
+
+twoBitToFa -bed="${bed_filename}" "${base_genome_2bit_file}" "${fasta_filename}" || exit 1;
+
 
 # Liftover procedure ...
 printf '\nLiftover procedure ...\n\n';
-${DATADIR}/genomes/generate-liftover-fasta.sh ${bed_filename} \
-		${base_genome_id} ${output_dir} $(create_region_description ${upstream_extension_in_bp} ${delineation} ${downstream_extension_in_bp} ${intronic_extension_in_bp}) ${genomes_liftover_table}
-		
+
+bash generate-liftover-fasta.sh \
+    "${bed_filename}" \
+	"${base_genome_id}" \
+	"${output_dir}" \
+	$(create_region_description \
+	    "${upstream_extension_in_bp}" \
+	    "${delineation}" \
+	    "${downstream_extension_in_bp}" \
+	    "${intronic_extension_in_bp}" \
+	) \
+	"${genomes_liftover_table}" || exit 1;
+
+
 # Copy ini file to output folder ...
-cp ${ini_filename} ${output_dir}
+cp "${ini_filename}" "${output_dir}";
